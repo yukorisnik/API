f11a4118fe6e16a4f9bb121a7ce4bb497cb251c6
yukorisnik <starcooler2@yahoo.se>
2026-01-11 10:27:39 +0100

    name change

--- src/test/java/systementor/integrationApiTestDemo/storeApiStatusTest.java
+++ src/test/java/systementor/integrationApiTestDemo/storeApiStatusTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 
-public class storeApiStatusTest {
+public class StoreApiStatusTest {
 
     @Test
     void callReturn200() {
@@ -12,6 +12,7 @@ public class storeApiStatusTest {
 
         var response = client.get()
                 .uri("/products")
+                .header("User-Agent", "GitHubActionsTest")
                 .exchangeToMono(r -> r.toEntity(String.class))
                 .block();
 
