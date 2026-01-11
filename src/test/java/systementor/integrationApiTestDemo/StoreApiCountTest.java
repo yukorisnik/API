1ed849b929db4b3a08c2495ec4663eef977c2819
yukorisnik <starcooler2@yahoo.se>
2026-01-11 12:47:44 +0100

    two tests

--- src/test/java/systementor/integrationApiTestDemo/StoreApiCountTest.java
+++ src/test/java/systementor/integrationApiTestDemo/StoreApiCountTest.java
@@ -16,7 +16,7 @@ public class StoreApiCountTest {
 
         List<Map<String, Object>> products = client.get()
                 .uri("/products")
-                .header("User-Agent", "GitHubActionsTest")
+                //.header("User-Agent", "GitHubActionsTest")
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<List<Map<String, Object>>>() {})
                 .block();
@@ -27,8 +27,7 @@ public class StoreApiCountTest {
 
         // Kontrollera antalet element i listan
         assertEquals(20, products.size());
-        //assertTrue(products.size() > 0);
-
+        
         System.out.println("Antal ID:n hittade: " + products.size() +", test ok");
     }
 }
