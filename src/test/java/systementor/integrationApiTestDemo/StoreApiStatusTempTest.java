d6c499bad3a85393966e1ab0a876661edf383393
yukorisnik <starcooler2@yahoo.se>
2026-01-11 10:43:39 +0100

    new name

---
+++ src/test/java/systementor/integrationApiTestDemo/StoreApiStatusTempTest.java
@@ -0,0 +1,26 @@
+package systementor.integrationApiTestDemo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class StoreApiStatusTempTest {
+
+    @Test
+    void callReturn200() {
+        WebClient client = WebClient.create("https://fakestoreapi.com");
+
+        var response = client.get()
+                .uri("/products")
+                .header("User-Agent", "GitHubActionsTest")
+                .exchangeToMono(r -> r.toEntity(String.class))
+                .block();
+
+        if (response == null) throw new AssertionError();
+        assertEquals(200, response.getStatusCode().value());
+
+        System.out.println("Status: " + response.getStatusCode());
+        //System.out.println("Body: " + response.getBody());
+    }
+
+}
