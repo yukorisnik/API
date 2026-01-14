d26b9578403d6fed30f1728b969413644879ae4d
yukorisnik <starcooler2@yahoo.se>
2026-01-14 17:30:40 +0100

    three tests API

---
+++ src/test/java/systementor/integrationApiTestDemo/StoreApiFieldsTest.java
@@ -0,0 +1,32 @@
+package systementor.integrationApiTestDemo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+
+    public class StoreApiFieldsTest {
+        WebClient client = WebClient.create("https://fakestoreapi.com");
+    
+        @Test
+        void verifyNumberOfIds() {var response = client.get()
+
+            .uri("/products/3")
+            .retrieve()
+            .toEntity(String.class)
+            .block();
+
+            assertEquals(200, response.getStatusCode().value());
+
+        String body = response.getBody();
+
+        assertTrue(body.contains("\"title\""));
+        assertTrue(body.contains("\"price\""));
+        assertTrue(body.contains("\"category\""));   
+
+        System.out.println("korrekta fält finns: " + body);
+     }
+}
+
+       
+
